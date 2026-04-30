4502e74d0cae6303ab51d2228f5faceb942bcd48
Santiago033 <santidia@nmsu.edu>
2026-04-29 20:57:46 -0600

    Update LabProgram.java

--- LabProgram.java
+++ LabProgram.java
@@ -1,16 +1,35 @@
 import java.util.Scanner;
 
 public class LabProgram {
-   
-   public static int fibonacci(int n) {
-      /* Type your code here. */
+
+   // Method to count frequency of a word (case insensitive)
+   public static int getWordFrequency(String[] wordsList, int listSize, String currWord) {
+      int count = 0;
+
+      for (int i = 0; i < listSize; i++) {
+         if (wordsList[i].equalsIgnoreCase(currWord)) {
+            count++;
+         }
+      }
+
+      return count;
    }
-   
+
    public static void main(String[] args) {
       Scanner scnr = new Scanner(System.in);
-      int startNum;
-      
-      startNum = scnr.nextInt();
-      System.out.println("fibonacci(" + startNum + ") is " + fibonacci(startNum));
+
+      int numWords = scnr.nextInt(); // number of words
+      String[] words = new String[numWords];
+
+      // Read words into array
+      for (int i = 0; i < numWords; i++) {
+         words[i] = scnr.next();
+      }
+
+      // Print each word with its frequency
+      for (int i = 0; i < numWords; i++) {
+         int freq = getWordFrequency(words, numWords, words[i]);
+         System.out.println(words[i] + " " + freq);
+      }
    }
 }
